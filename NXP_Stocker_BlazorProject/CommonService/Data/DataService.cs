9de48d4f0e0894e21a681e9cb8cff2ba135a7a74
Brian.Chen <Brian.Chen@A11130042PC>
2025-10-29 17:14:01 +0800

    新增Common Service

---
+++ NXP_Stocker_BlazorProject/CommonService/Data/DataService.cs
@@ -0,0 +1,12 @@
+﻿using NXP_Stocker_BlazorProject.CommonService.Data.Interface;
+
+namespace NXP_Stocker_BlazorProject.CommonService.Data
+{
+    public partial class DataService : IDataService
+    {
+        public DataService()
+        {
+
+        }
+    }
+}
---
+++ NXP_Stocker_BlazorProject/CommonService/Observer/ObserverService.cs
@@ -0,0 +1,40 @@
+﻿using CommonLibraryB.Tools.LogWritter;
+
+namespace NXP_Stocker_BlazorProject.CommonService.Observer
+{
+    public partial class ObserverService : INLogWritterObservable
+    {
+        List<INLogWritterObserver> os { get; set; }
+
+        public void AddNLogWritterObserver(INLogWritterObserver o)
+        {
+            if(os == null)
+                os = new List<INLogWritterObserver>();
+
+            if(!os.Contains(o))
+            {
+                os.Add(o);
+            }
+        }
+
+        public void RemoveNLogWritterObserver(INLogWritterObserver o)
+        {
+            if(os != null && os.Contains(o))
+            {
+                os.Remove(o);
+            }
+        }
+
+        public async Task NotifyNLog(EStatus status, string msg)
+        {
+            if(os != null)
+            {
+                foreach(var o in os)
+                {
+                    await o.WriteNLog(status, msg);
+                }
+            }
+        }
+
+    }
+}
