1085c8ccfbce2ea68b191395cb32f8d9ee378780
Brian.Chen <Brian.Chen@A11130042PC>
2025-10-29 16:33:29 +0800

    新增PierTaskPack

---
+++ NXP_Stocker_BlazorProject/TaskPackage/PierTaskPackage/Interface/IPierTaskPack.cs
@@ -0,0 +1,7 @@
+﻿namespace NXP_Stocker_BlazorProject.TaskPackage.PierTaskPackage.Interface
+{
+    public interface IPierTaskPack
+    {
+
+    }
+}
---
+++ NXP_Stocker_BlazorProject/TaskPackage/PierTaskPackage/PierTaskPack.cs
@@ -0,0 +1,21 @@
+﻿using CommonLibraryB_NXP.Library.PLC;
+using CommonLibraryB_NXP.Library.PLC.Adapter;
+
+namespace NXP_Stocker_BlazorProject.TaskPackage.PierTaskPackage
+{
+
+    public partial class PierTaskPack<EPLC>
+    {
+        readonly EPLC pier;
+
+        readonly IPlcOperate<EPLC> IPeirOp;
+
+        readonly PlcLibrary<EPLC> pierLib;
+
+        public PierTaskPack()
+        {
+
+        }
+
+    }
+}
