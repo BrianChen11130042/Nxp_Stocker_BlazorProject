8ac208ecdb45cba3409a6183d4a23324b4c26946
Brian.Chen <Brian.Chen@A11130042PC>
2025-11-03 13:15:03 +0800

    新增robot task

---
+++ NXP_Stocker_BlazorProject/Tasks/RobotTask.cs
@@ -0,0 +1,70 @@
+﻿using CommonLibraryB.Base.FiniteStateMachine;
+using NXP_Stocker_BlazorProject.TaskPackage.RobotTaskPackage.Interface;
+
+namespace NXP_Stocker_BlazorProject.Tasks
+{
+
+    public partial class RobotTask : IRobotTaskPack
+    {
+        readonly IRobotTaskPack pack;
+
+        public RobotTask(IRobotTaskPack pack)
+        {
+            this.pack = pack;
+            interval = 10;
+        }
+    }
+
+    public enum ERobotAction
+    {
+        None,
+        CheckMission,
+        Start,
+        Result,
+        Success,
+        Fail,
+    }
+
+    public partial class RobotTask : FSMBase<ERobotAction, int>
+    {
+        public async override Task Action()
+        {
+            key = EHandshakeKey.Run;
+
+            switch(S2)
+            {
+                case ERobotAction.None:
+                    Set(ES1.Finish, ERobotAction.None, 0);
+                    break;
+
+                case ERobotAction.CheckMission:
+
+                    break;
+            }
+        }
+
+        public async override Task Error()
+        {
+            isError = true;
+            key = EHandshakeKey.Finish;
+            Set(ES1.Idle, ERobotAction.None, 0);
+        }
+
+        public async override Task Finish()
+        {
+            isError = false;
+            key = EHandshakeKey.Finish;
+            Set(ES1.Idle, ERobotAction.None, 0);
+        }
+
+        public async override Task Idle()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public async override Task Init()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
