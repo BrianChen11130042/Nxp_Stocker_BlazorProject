52fb072c03dd9937df98a25d945fef618c34f2e6
Brian.Chen <Brian.Chen@A11130042PC>
2025-11-04 18:04:10 +0800

    修改mission asign task

--- NXP_Stocker_BlazorProject/Tasks/MissionAssignTask.cs
+++ NXP_Stocker_BlazorProject/Tasks/MissionAssignTask.cs
@@ -1,12 +1,55 @@
-﻿namespace NXP_Stocker_BlazorProject.Tasks
-{
-    public partial class MissionAssignTask
-    {
+﻿using CommonLibraryB.Base.FiniteStateMachine;
+using NXP_Stocker_BlazorProject.TaskPackage.MissionAssignTaskPackage.Interface;
 
+namespace NXP_Stocker_BlazorProject.Tasks
+{
+    public partial class MissionAssignTask : IMissionAsignTaskPack
+    {
+        readonly IMissionAsignTaskPack pack;
+
+        public MissionAssignTask(IMissionAsignTaskPack pack)
+        {
+            this.pack = pack;
+            interval = 10;
+        }
+
+        
     }
 
-    public partial class MissionAssignTask
+    public enum EMission
     {
+        None,
+        CheckMission,
+        InputWarehouse,
+        OutputWarehouse,
+        TransformWarehouse
+    }
 
+    public partial class MissionAssignTask : FSMBase<EMission, int>
+    {
+        public override Task Action()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task Error()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task Finish()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task Idle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task Init()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
