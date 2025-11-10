a03d8a76a0916d67e0ed2000d0e726372b091a57
Brian.Chen <Brian.Chen@A11130042PC>
2025-11-10 17:43:54 +0800

    machineService 新增獲取modbustcp config

---
+++ NXP_Stocker_BlazorProject/Services/Interface/IMachineService.cs
@@ -0,0 +1,11 @@
+﻿using CommonLibraryB_NXP.Manager.ModbusTcp.Master;
+
+namespace NXP_Stocker_BlazorProject.Services.Interface
+{
+
+    public interface IMachineService
+    {
+        //Config
+        Task<List<ModbusTcpMasterConfig>> GetModbusTcpConfig();
+    }
+}
--- NXP_Stocker_BlazorProject/Services/MachineService.cs
+++ NXP_Stocker_BlazorProject/Services/MachineService.cs
@@ -1,4 +1,6 @@
-﻿using NXP_Stocker_BlazorProject.Scope;
+﻿using CommonLibraryB_NXP.Manager.ModbusTcp.Master;
+using NXP_Stocker_BlazorProject.Scope;
+using NXP_Stocker_BlazorProject.Services.Interface;
 
 namespace NXP_Stocker_BlazorProject.Services
 {
@@ -12,8 +14,23 @@ namespace NXP_Stocker_BlazorProject.Services
         }
     }
 
-    public partial class MachineService
+    public partial class MachineService : IMachineService
     {
+        public async Task<List<ModbusTcpMasterConfig>> GetModbusTcpConfig()
+        {
+            List<ModbusTcpMasterConfig> list = new List<ModbusTcpMasterConfig>();
 
+            foreach(string dev in Enum.GetNames(typeof(EModbusTcpMaster)))
+            {
+                ModbusTcpMasterConfig config = scope.modbusTcpMasterManager.Get(dev);
+
+                if(config != null)
+                {
+                    list.Add(config);
+                }
+            }
+
+            return list;
+        }
     }
 }
